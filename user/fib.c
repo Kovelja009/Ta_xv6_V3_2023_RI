3214d31a7ded4021665f5fc2b2b5a4b7aa0e3217
Vanja Kovinic <vanjakovinic009@gmail.com>
2023-05-02 09:45:33 +0200

    updated

--- user/fib.c
+++ user/fib.c
@@ -3,17 +3,6 @@
 #include "user.h"
 #include "kernel/fs.h"
 
-int isPrime(int num){
-    int flag=1;
-    for(int i = 2; i <= num / 2; i++) {
-       if(num % i == 0) {
-          flag = 0;
-          break;
-       }
-    }
-    return flag;
-}
-
 int
 main(int argc, char *argv[])
 {
